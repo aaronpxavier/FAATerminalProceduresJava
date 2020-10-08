15040e47c5d617c613e50945b7cd417e7c87d644
Arun Mavumkal <av8r88@gmail.com>
2020-10-07 23:20:10 -0400

    initial scafolding

---
+++ src/com/fltprep/charts/TerminalProcedures.java
@@ -0,0 +1,7 @@
+package charts;
+
+public class TerminalProcedures extends Chart {
+    public static void getCharts() {
+
+    }
+}
---
+++ src/com/fltprep/dttp/DttpBulk.java
@@ -0,0 +1,12 @@
+package dttp;
+
+public class DttpBulk {
+    public static void dloadMetaFile(String dest_dir) {
+        //**todo**//
+    }
+
+    public static String getCurrentCycle() {
+        //**todo**//
+        return "";
+    }
+}
