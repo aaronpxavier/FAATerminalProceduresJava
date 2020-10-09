0ec11939ec02ba59d32c452b576d7064cde97960
Arun Mavumkal <av8r88@gmail.com>
2020-10-08 21:21:54 -0400

    added getcurrentcycle method to dttp and wrote junit test for method

--- src/main/java/com/fltprep/dttp/DttpBulk.java
+++ src/main/java/com/fltprep/dttp/DttpBulk.java
@@ -1,16 +1,56 @@
 package com.fltprep.dttp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class DttpBulk {
+    public static final String FAA_CYCLE_REF_DATE_STRING = "2020-01-02";
+    public static final Date FAA_CYCLE_REF_DATE = getFAARefDate();
+
+    private static Date getFAARefDate() {
+        Date refDate = null;
+        try {
+            SimpleDateFormat df;
+            df = new SimpleDateFormat("yyyy-MM-dd");
+            refDate = df.parse(FAA_CYCLE_REF_DATE_STRING);
+        }
+        catch (Exception e) {
+            System.err.println(e);
+        }
+        return refDate;
+    }
+
     public static void dloadMetaFile(String dest_dir) {
         //**todo**//
     }
 
+    /**
+     * @return String returns cycle string used in url for faa dttp zip files;
+     */
     public static String getCurrentCycle() {
-        //**todo**//
-        return "";
+        Calendar resultTimeCal = Calendar.getInstance();
+        int month;
+        int year;
+        int day;
+        String currentCycleString = "";
+
+        // loops from reference date until
+        while (resultTimeCal.getTime().compareTo(FAA_CYCLE_REF_DATE) <= 0)
+            resultTimeCal.add(Calendar.DATE, 28);
+
+        if (resultTimeCal.getTime().compareTo(FAA_CYCLE_REF_DATE) != 0)
+            resultTimeCal.add(Calendar.DATE, -28);
+
+        month = resultTimeCal.get(Calendar.MONTH);
+        year = resultTimeCal.get(Calendar.YEAR);
+        day = resultTimeCal.get(Calendar.DAY_OF_MONTH);
+
+        currentCycleString = String.valueOf(year).substring(2);
+        currentCycleString += day < 10 ? "0" + String.valueOf(day) : String.valueOf(day);
+        currentCycleString += month < 10 ? "0" + String.valueOf(month) : String.valueOf(month);
+
+        return currentCycleString;
     }
 
-    public static String helloWorld() {
-        return "Hello World";
-    }
 }
--- src/test/java/com/fltprep/dttp/DttpBulkTest.java
+++ src/test/java/com/fltprep/dttp/DttpBulkTest.java
@@ -7,9 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DttpBulkTest {
     DttpBulk tester = new DttpBulk();
-    @DisplayName("Test dttp.testDttpHelloWorld()")
     @Test
-    public void testDttpHelloWorld() {
-        assertEquals("Hello World", tester.helloWorld());
+    @DisplayName("Test for DttpBulk.getCurrentCycle()")
+    void getCurrentCycle() {
+        String testString = tester.getCurrentCycle();
+        assertEquals(true, testString.matches("[0-9]{6}"));
     }
 }
